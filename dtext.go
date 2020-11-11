24ef59f943bd9c4e7eb724ef0bf166ac607bd2fa
Romans Davidko <br.fearfree@gmail.com>
2020-11-11 17:19:03 +0200

    added unified Start() func to handle all elements. Added exitChannel to control goroutine lifecycle

--- dtext.go
+++ dtext.go
@@ -4,9 +4,27 @@ import (
 "time"
 )
 
-func Spiner(ch chan string, p int) {
+func Start(element string, ch chan string, p int) chan bool{
+	controlChannel := make(chan bool)
+	switch element {
+	case "spinner":
+			go Spinner(ch, p, controlChannel)
+	default:
+	}
+	return controlChannel
+}
+
+func Spinner(ch chan string, p int, outCh chan bool){
 	x := "-"
 	for {
+
+		select{
+		case <- outCh:
+			ch <- ""
+			return;
+		default:
+		}
+
 		ch <- x
 		switch x {
 		case "-":
